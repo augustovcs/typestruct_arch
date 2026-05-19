52efc4f7cc003a185093fddbeed7099b4da5060b
Joao V <augustoviegascs@gmail.com>
2026-05-19 01:39:17 -0300

    added routes adjst

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,19 @@
 import "./styles.css"
 import { CardReact } from "../../shared/ui/Cards";
+import { mockCard } from "../../shared/mocks/cardMock";
 
 export function DashboardPage() {
     return (
         <div>
             <h1> Dashboard Principal Modulo </h1>
             <div className="cards-grid">
-                <CardReact title="T1" description="T2" value="2"/>
-                <CardReact title="T1" description="T2" value="2"/>
-                <CardReact title="T1" description="T2" value="2"/>
-                <CardReact title="T1" description="T2" value="2"/>
+                {mockCard.map((card) => (
+                    <CardReact 
+                    title={card.title}
+                    description={card.description}
+                    value={card.value}
+                    />
+                ))}
             </div>
            
         </div>
---
+++ src/shared/mocks/cardMock.ts
@@ -0,0 +1,21 @@
+export const mockCard = [
+    {
+    title: "Users",
+    description: "Total users",
+    value: "120"
+    },
+
+    {
+    title: "Revenue",
+    description: "Monthly revenue",
+    value: "$5.000"
+     },
+
+     {
+    title: "Orders",
+    description: "Total orders",
+    value: "85"
+    }
+
+    
+];
